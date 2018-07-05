f8c4226539f0315aa87b329bdf0cd2a83983c038
bbosak1 <33043855+bbosak1@users.noreply.github.com>
2018-07-04 20:25:07 -0500

    Committing first draft of week 1 programs
    -Brad Bosak

--- spam_friends.py
+++ spam_friends.py
@@ -1,2 +1,9 @@
-for i in range(0,100):
-    print("I will never spam my friends again")
+#Brad Bosak, spam_friends program
+
+#This is a program that writes out a sentence 100 times
+def main():
+    
+    for i in range(0,100):
+        print("I will never spam my friends again.")
+
+main()
