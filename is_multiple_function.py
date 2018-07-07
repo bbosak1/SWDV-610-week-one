bf07e117290aa0cc9c208df0b0b6d64eae2cc580
bbosak1 <33043855+bbosak1@users.noreply.github.com>
2018-07-07 08:49:33 -0500

    Final submission for week 1
    -Brad Bosak

--- is_multiple_function.py
+++ is_multiple_function.py
@@ -4,7 +4,7 @@
 def is_multiple(n, m):
    "multiples description"
    if m % n == 0:
-       print("True")   #The function will display "True"
+       print("True")   #The function will display "True" if n is a multiple of m
        return True     #The function will return True as well.  I did this to demonstrate the return feature of a function
    else:
        print("False")  #Likewise, the other part of the control flow will print and return False
--- spam_friends.py
+++ spam_friends.py
@@ -3,7 +3,7 @@
 #This is a program that writes out a sentence 100 times
 def main():
     
-    for i in range(0,100):
+    for i in range(0,100): #range determines how many times the below sentence will print to the console
         print("I will never spam my friends again.")
 
 main()
